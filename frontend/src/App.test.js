8976ce8088d90fee15fdcfabede1111dfdde5ba2
August14Er <ls4augsto8er@gmail.com>
2026-04-22 17:35:16 -0500

    Actualizar prueba por defecto de React a LabSync

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders login page', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  // Ahora la prueba buscará la palabra "LabSync" en lugar de "Learn React"
+  const textElement = screen.getByText(/LabSync/i);
+  expect(textElement).toBeInTheDocument();
 });
