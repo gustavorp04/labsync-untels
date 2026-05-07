9844c7dc5b7269da3573b8ccb06ba65f1c4653b9
August14Er <ls4augsto8er@gmail.com>
2026-05-06 20:16:56 -0500

    feat: habilitar visibilidad de modelos en el panel de admin

--- backend/reservas/admin.py
+++ backend/reservas/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.apps import apps
 
-# Register your models here.
+# Registra dinámicamente todos los modelos de la app 'reservas'
+app = apps.get_app_config('reservas')
+
+for model_name, model in app.models.items():
+    try:
+        admin.site.register(model)
+    except admin.sites.AlreadyRegistered:
+        pass
