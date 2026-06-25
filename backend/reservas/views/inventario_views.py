ca5064901d2dbf88bc205a1ef1bf03c938334609
unknown <2223010469@untels.edu.pe>
2026-06-24 21:05:19 -0500

    Correccion extra

--- backend/reservas/views/inventario_views.py
+++ backend/reservas/views/inventario_views.py
@@ -1,6 +1,6 @@
 from rest_framework import generics, status
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from django.utils import timezone
 from django.core.cache import cache
